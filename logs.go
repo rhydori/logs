e9732356e611391048b14ec24c6ff71596a09fdb
Rhydor <115670922+Rhydori@users.noreply.github.com>
2026-01-12 21:11:11 -0300

    Almost no allocation refactor

--- logs.go
+++ logs.go
@@ -2,8 +2,8 @@ package logs
 
 import (
 	"fmt"
+	"io"
 	"os"
-	"strings"
 	"sync"
 	"time"
 
@@ -11,93 +11,136 @@ import (
 	"golang.org/x/term"
 )
 
-type logger struct {
-	logChan   chan logEntry
-	wg        sync.WaitGroup
-	closeOnce sync.Once
-}
-
-type logColors struct {
-	reset  string
-	red    string
-	green  string
-	yellow string
-	blue   string
-	purple string
-	cyan   string
-}
-
 type logEntry struct {
-	level string
-	color string
-	msg   string
+	level []byte
+	color []byte
+	buf   []byte
 }
 
-var rlog = logger{
-	logChan: make(chan logEntry, 128),
-}
-var colors logColors
+var out io.Writer = os.Stdout
+
+var (
+	logChan = make(chan *logEntry, 1024)
+	bufPool = sync.Pool{
+		New: func() any {
+			b := make([]byte, 0, 256)
+			return &b
+		},
+	}
+	reset = []byte("")
+
+	red    = []byte("")
+	green  = []byte("")
+	yellow = []byte("")
+	//blue   = []byte("")
+	purple = []byte("")
+	cyan   = []byte("")
+
+	lDEBUG = []byte("DEBUG")
+	lINFO  = []byte("INFO")
+	lWARN  = []byte("WARN")
+	lERROR = []byte("ERROR")
+	lFATAL = []byte("FATAL")
+)
 
 func init() {
 	noColorEnv := os.Getenv("NO_COLOR") == "true"
 	if !noColorEnv || term.IsTerminal(int(os.Stdout.Fd())) {
 		ansi.EnableANSI()
-
-		colors = logColors{
-			reset:  "\033[0m",
-			red:    "\033[31m",
-			green:  "\033[32m",
-			yellow: "\033[33m",
-			blue:   "\033[34m",
-			purple: "\033[35m",
-			cyan:   "\033[36m",
-		}
 	}
 
-	rlog.wg.Add(1)
-	go func() {
-		defer rlog.wg.Done()
-		for entry := range rlog.logChan {
-			now := time.Now().Format("[15:04:05.000]")
-			fmt.Printf("%s%s [%s] %s%s\n", entry.color, now, entry.level, entry.msg, colors.reset)
-		}
-	}()
+	go writer()
 }
 
-func trySend(entry logEntry) {
+func writer() {
+	for e := range logChan {
+		out.Write(e.buf)
+		bufPool.Put(&e.buf)
+	}
+}
+
+func SetOutput(w io.Writer) {
+	out = w
+}
+
+func log(level, color []byte, msg string) {
+	bp := bufPool.Get().(*[]byte)
+	buf := (*bp)[:0]
+
+	now := time.Now()
+	buf = append(buf, color...)
+	buf = append(buf, '[')
+	appendTime(&buf, now)
+	buf = append(buf, "] ["...)
+	buf = append(buf, level...)
+	buf = append(buf, "] "...)
+	buf = append(buf, msg...)
+	buf = append(buf, reset...)
+	buf = append(buf, '\n')
+
+	*bp = buf
 	select {
-	case rlog.logChan <- entry:
+	case logChan <- &logEntry{buf: buf}:
 	default:
-		os.Stderr.WriteString("rlog: channel full, dropping log\n")
 	}
 }
 
-func sendLog(level, color string, parts ...any) {
-	full := strings.TrimSpace(fmt.Sprintln(parts...))
-	trySend(logEntry{level, color, full})
+func logf(level, color []byte, format string, args ...any) {
+	bp := bufPool.Get().(*[]byte)
+	buf := (*bp)[:0]
+
+	now := time.Now()
+	buf = append(buf, color...)
+	buf = append(buf, '[')
+	appendTime(&buf, now)
+	buf = append(buf, "] ["...)
+	buf = append(buf, level...)
+	buf = append(buf, "] "...)
+
+	buf = fmt.Appendf(buf, format, args...)
+
+	buf = append(buf, reset...)
+	buf = append(buf, '\n')
+
+	*bp = buf
+	select {
+	case logChan <- &logEntry{buf: buf}:
+	default:
+	}
 }
 
-func sendLogf(level, color, msg string, args ...any) {
-	full := fmt.Sprintf(msg, args...)
-	trySend(logEntry{level, color, full})
+func append2(b *[]byte, v int) {
+	*b = append(*b, byte('0'+v/10), byte('0'+v%10))
 }
 
-func Debug(args ...any) { sendLog("DEBUG", colors.cyan, args...) }
-func Info(args ...any)  { sendLog("INFO", colors.green, args...) }
-func Warn(args ...any)  { sendLog("WARN", colors.yellow, args...) }
-func Error(args ...any) { sendLog("ERROR", colors.purple, args...) }
-func Fatal(args ...any) { sendLog("FATAL", colors.red, args...); Close() }
-
-func Debugf(msg string, args ...any) { sendLogf("DEBUG", colors.cyan, msg, args...) }
-func Infof(msg string, args ...any)  { sendLogf("INFO", colors.green, msg, args...) }
-func Warnf(msg string, args ...any)  { sendLogf("WARN", colors.yellow, msg, args...) }
-func Errorf(msg string, args ...any) { sendLogf("ERROR", colors.purple, msg, args...) }
-func Fatalf(msg string, args ...any) { sendLogf("FATAL", colors.red, msg, args...); Close() }
-
-func Close() {
-	rlog.closeOnce.Do(func() {
-		close(rlog.logChan)
-		rlog.wg.Wait()
-		os.Exit(0)
-	})
+func append3(b *[]byte, v int) {
+	*b = append(*b,
+		byte('0'+v/100),
+		byte('0'+(v/10)%10),
+		byte('0'+v%10),
+	)
 }
+
+func appendTime(b *[]byte, t time.Time) {
+	h, m, s := t.Hour(), t.Minute(), t.Second()
+	ms := t.Nanosecond() / 1e6
+	append2(b, h)
+	*b = append(*b, ':')
+	append2(b, m)
+	*b = append(*b, ':')
+	append2(b, s)
+	*b = append(*b, '.')
+	append3(b, ms)
+}
+
+func Debug(msg string) { log(lDEBUG, cyan, msg) }
+func Info(msg string)  { log(lINFO, green, msg) }
+func Warn(msg string)  { log(lWARN, yellow, msg) }
+func Error(msg string) { log(lERROR, purple, msg) }
+func Fatal(msg string) { log(lFATAL, red, msg); os.Exit(0) }
+
+func Debugf(format string, args ...any) { logf(lDEBUG, cyan, format, args...) }
+func Infof(format string, args ...any)  { logf(lINFO, green, format, args...) }
+func Warnf(format string, args ...any)  { logf(lWARN, yellow, format, args...) }
+func Errorf(format string, args ...any) { logf(lERROR, purple, format, args...) }
+func Fatalf(format string, args ...any) { logf(lFATAL, red, format, args...); os.Exit(0) }
--- logs_bench_test.go
+++ logs_bench_test.go
@@ -1,11 +1,36 @@
-package logs
+package logs_test
 
 import (
 	"testing"
+
+	"github.com/rhydori/logs"
 )
 
-func BenchmarkLogger(b *testing.B) {
+type discardWriter struct{}
+
+var test = "TESTING"
+
+func (discardWriter) Write(p []byte) (int, error) {
+	return len(p), nil
+}
+
+func BenchmarkLog(b *testing.B) {
+	logs.SetOutput(discardWriter{})
+
+	b.ReportAllocs()
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Info("hello world", i)
+		logs.Info("hello world" + test)
+	}
+}
+
+func BenchmarkLogf(b *testing.B) {
+	logs.SetOutput(discardWriter{})
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		logs.Infof("hello world %s", test)
 	}
 }
